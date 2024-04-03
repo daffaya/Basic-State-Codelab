8734643514ee1a0336e896083d74678efdf62e37
daffaya <daffapasya21@gmail.com>
2024-04-03 10:22:37 +0700

    done

---
+++ app/src/main/java/com/kumaa/basicstatecodelab/WellnessViewModel.kt
@@ -0,0 +1,25 @@
+package com.kumaa.basicstatecodelab
+
+import androidx.compose.runtime.toMutableStateList
+import androidx.lifecycle.ViewModel
+
+class WellnessViewModel : ViewModel() {
+    private val _tasks = getWellnessTasks().toMutableStateList()
+    val tasks: List<WellnessTask>
+        get() = _tasks
+
+    fun remove(item: WellnessTask) {
+        _tasks.remove(item)
+    }
+
+    fun changeTaskChecked(
+        item: WellnessTask,
+        checked: Boolean,
+    ) {
+        _tasks.find { it.id == item.id }?.let { task ->
+            task.checked = checked
+        }
+    }
+}
+
+private fun getWellnessTasks() = List(15) { i -> WellnessTask(i, "Task # $i") }
